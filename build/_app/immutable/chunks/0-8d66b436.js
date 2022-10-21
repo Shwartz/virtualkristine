d6605de486e1f32d7924a3a850f288fcd41257c3
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-10-21 20:19:25 +0100

    update

---
+++ build/_app/immutable/chunks/0-8d66b436.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-2ae54982.js";export{t as component};
--- build/_app/immutable/chunks/0-aeec7f29.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-b9b5ccd1.js";export{t as component};
