0cca1a9b0633b9309e362859436810df7f0edd0b
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-10-21 20:08:12 +0100

    adding build to gitignore

---
+++ build/_app/immutable/chunks/0-aeec7f29.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-b9b5ccd1.js";export{t as component};
---
+++ build/_app/immutable/chunks/1-2632fabb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-d5c49f7a.js";export{t as component};
